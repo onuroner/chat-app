5a6f19f2727cc041e6a841176b0e545598f857b2
Onur Öner <onuroner4054@gmail.com>
2024-12-10 21:51:11 +0300

    Check auth binding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route } from "react-router-dom"
+import { Routes,Route, Navigate } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import SignUpPage from "./pages/SignUpPage"
@@ -7,9 +7,10 @@ import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
+import { Oval } from 'react-loader-spinner'
 
 const App = () => {
-  const {authUser, checkAuth} = useAuthStore();
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
   useEffect(() => {
     checkAuth();
@@ -18,15 +19,29 @@ const App = () => {
 
   console.log({authUser})
 
+  if(isCheckingAuth && !authUser){
+    return (<div className="flex items-center justify-center h-screen">
+      <Oval
+  visible={true}
+  height="50"
+  width="50"
+  color="#4fa94d"
+  ariaLabel="oval-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />
+    </div>)
+  }
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to={"/login"}/>} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to={"/"}/> } />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to={"/"}/>} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to={"/login"}/>} />
       </Routes>
     </div>
   )
