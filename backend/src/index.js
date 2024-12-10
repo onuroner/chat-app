e745c7cf40c9c807a37298a5847ef7faf3852707
Onur Öner <onuroner4054@gmail.com>
2024-12-10 17:21:21 +0300

    Message routes added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import {connectDB} from "./lib/db.js"
 
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json()); //allow you to extract the json data out of body
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, ()=> {
     console.log('Server is listening on port ' + PORT);
