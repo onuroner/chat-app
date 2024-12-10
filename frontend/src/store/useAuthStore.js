f25a0cbfbd9927ade8b1074a39e8c565c2c72f4c
Onur Öner <onuroner4054@gmail.com>
2024-12-11 00:03:14 +0300

    Login page created

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,7 +6,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp:false,
-    isLoggingIng:false,
+    isLoggingIn:false,
     isUpdatingProfile:false,
 
     checkAuth: async() => {
@@ -45,5 +45,18 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         } 
+    },
+
+    login: async(data)=>{
+        set({isLoggingIn:true});
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({authUser:res.data});
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isLoggingIn:false});
+        }
     }
 }));
