da0d5d6f315d174bd852d1276841f32e4ffc3fd3
Onur Öner <onuroner4054@gmail.com>
2024-12-10 23:26:37 +0300

    SignUp page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import ProfilePage from "./pages/ProfilePage"
 import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
 import { Oval } from 'react-loader-spinner'
+import { Toaster } from "react-hot-toast"
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
@@ -43,6 +44,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to={"/login"}/>} />
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
